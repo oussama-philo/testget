d04f2e827fd15d3869aeaca7b2de56da29d8d606
Oussama Lachhab (Philo) <ousamalechheb@gmail.com>
2024-11-21 00:45:31 +0100

    str join new

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -53,22 +53,19 @@ char *ft_strjoin(char const *s1, char const *s2)
 	char *str_total;
 
 	if (!s1 && !s2)
-	{
 		return (NULL);
-	}
 	if (!s1)
-	{
 		return (ft_strdup(s2));
-	}
 	if (!s2)
-	{
 		return (ft_strdup(s1));
-	}
 	total_len = ft_strlen(s1) + ft_strlen(s2);
 	str_total = malloc((total_len + 1) * sizeof(char));
 	if (!str_total)
-	{
 		return (NULL);
+	while (total_len)
+	{
+		*str_total++ = 0;
+		total_len--;
 	}
 	ft_memcpy(str_total, s1, ft_strlen(s1));
 	ft_memcpy(str_total + ft_strlen(s1), s2, ft_strlen(s2));
